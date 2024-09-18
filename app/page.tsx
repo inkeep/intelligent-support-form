5e6497eece36a58673f3f71b44fb0d1fc51bd7b7
omar-inkeep <omar@inkeep.com>
2024-09-18 14:45:36 -0700

    maxDuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { AI } from "./ai/IntelligentFormAIConfig";
 import IntelligentForm from "./form-components/IntelligentForm";
 
+export const maxDuration = 300;
+
 export default function Home() {
   return (
     <AI>
