b54ec7046253d3d8bc54ef9cfe7bc9ab6dc3e883
sarah_inkeep <sarah.niemiec@inkeep.com>
2024-09-27 17:02:04 -0700

    copy tweak

--- components/DemoBanner.tsx
+++ components/DemoBanner.tsx
@@ -5,9 +5,7 @@ export function DemoBanner() {
     <div className="bg-gray-50 border-b border-gray-200 p-2 text-gray-600 flex text-sm px-6">
       <div className="flex justify-between items-center w-full">
         <div className="flex-1 font-semibold mx-2 flex-shrink-0 whitespace-nowrap text-gray-500">Demo mode</div>
-        <div className="flex-auto lg:flex-2 mx-2 truncate text-center">
-          This is a demo, form data is not being collected.
-        </div>
+        <div className="flex-auto lg:flex-2 mx-2 truncate text-center">Form submissions will not be collected.</div>
         <div className="flex-1 mx-2 flex-shrink-0 flex items-center justify-end whitespace-nowrap">
           <a
             href="mailto:hi@inkeep.com?subject=Inkeep%20support%20copilot%20demo"
